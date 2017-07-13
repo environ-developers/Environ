96f4fbec76454b87fb896e7547773ee0f26bb93f
Oliviero Andreussi <oliviero.andreussi@epfl.ch>
2017-07-13 14:41:35 +0200

    added tests for environ
    followed the approach of QE using testcode. Added tests and benchmarks for surface and volume with sccs and soft-spheres.

---
+++ tests/run-pw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (C) 2001 Quantum ESPRESSO
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License. See the file `License' in the root directory
+# of the present distribution.
+
+#source ${ENVIRON_ROOT}/tests/ENVIRONMENT
+
+if [ $QE_USE_MPI == 1 ]; then
+  export PARA_PREFIX="mpirun -np ${TESTCODE_NPROCS}"
+else
+  unset PARA_PREFIX
+fi
+
+# Additional stuff before run special test-cases
+
+${PARA_PREFIX} ${ESPRESSO_ROOT}/bin/pw.x --environ "$@"
+
+if [ -f environ.debug ]; then
+  cat environ.debug
+  rm environ.debug
+fi
+
+rm -f input_tmp.in environ.in
