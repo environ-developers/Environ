6fa3e351cceef44ee7eea07c56d4a2f9cb72a060
Francesco Nattino <francesco.nattino@epfl.ch>
2019-04-15 15:29:55 +0200

    example on diffuse layer (example06) updated

--- examples/example06/run_example.sh
+++ examples/example06/run_example.sh
@@ -11,31 +11,34 @@ $ECHO
 $ECHO "$EXAMPLE_DIR : starting"
 $ECHO
 $ECHO "This example shows how to use pw.x to simulate a charged two-dimensional "
-$ECHO "system immersed in continuum solvent described by the SCCS model "
+$ECHO "system immersed in a continuum electrolyte solution with open boundary "
+$ECHO "conditions along the axis perpendicular to the slab plane, as described in "
 $ECHO
-$ECHO "   O. Andreussi, I. Dabo and N. Marzari, J. Chem. Phys. 136, 064102 (2012) "
+$ECHO "     O. Andreussi and N. Marzari, Phys. Rev. B 90, 245101 (2014). " 
 $ECHO
-$ECHO "and with open boundary conditions along the axis perpendicular "
-$ECHO "to the slab plane, as described in "
+$ECHO "The electrochemical diffuse layer is modeled by solving different flavors "
+$ECHO "of the Poisson-Boltzmann equation, see "
 $ECHO
-$ECHO "   O. Andreussi and N. Marzari, Phys. Rev. B 90, 245101 (2014) " 
+$ECHO "  I. Borukhov, D. Andelman, and H. Orland, Phys. Rev. Lett. 79, 435 (1997);"
+$ECHO "  I. Dabo, E. Cancès, Y.L. Li, and N. Marzari, arXiv 0901.0096 (2008); "
+$ECHO "      S. Ringe, H. Oberhofer, C. Hille, S. Matera, and K. Reuter, "
+$ECHO "                J. Comput. Theor. Chem. 12, 4052 (2016);"
+$ECHO "    G. Fisicaro, L. Genovese, O. Andreussi, N. Marzari and S. Goedecker,"
+$ECHO "                   J. Chem. Phys. 144, 014103 (2016). "
 $ECHO
-$ECHO "The presence of electrolytes in the solution is accounted for through"
-$ECHO "the linearized modified Poisson-Boltzmann model, see "
-$ECHO 
-$ECHO " I. Borukhov, D. Andelman, and H. Orland, Phys. Rev. Lett. 79, 435 (1997)"
-$ECHO "   I. Dabo, E. Cancès, Y.L. Li, and N. Marzari, arXiv 0901.0096 (2008) "
-$ECHO "       S. Ringe, H. Oberhofer, C. Hille, S. Matera, and K. Reuter, "
-$ECHO "            J. Comput. Theor. Chem. 12, 4052 (2016)."
-$ECHO "   G. Fisicaro, L. Genovese, O. Andreussi, N. Marzari and S. Goedecker,"
-$ECHO "                  J. Chem. Phys. 144, 014103 (2016) "
+$ECHO "Details on the implementation are described in"
+$ECHO
+$ECHO "          F. Nattino, M. Truscott, N. Marzari, O. Andreussi,"
+$ECHO "                   J. Chem. Phys. 150, 041722 (2019)."
+$ECHO
+
 
 # set the needed environment variables
 . ../../../environment_variables
 
 # required executables and pseudopotentials
 BIN_LIST="pw.x"
-PSEUDO_LIST="O.pbe-rrkjus.UPF C.pbe-rrkjus.UPF Pt.pbe-nd-rrkjus.UPF"
+PSEUDO_LIST="Ag.pbe-n-rrkjus_psl.1.0.0.UPF"
 
 $ECHO
 $ECHO "  executables directory: $BIN_DIR"
@@ -92,128 +95,164 @@ $ECHO "  running pw.x as: $PW_COMMAND"
 $ECHO
 
 ### ELECTROSTATIC EMBEDDING PARAMETERS ##############################
-verbose=0             # if GE 1 prints debug informations
+verbose=0             # if GE 1 prints debug informations;
                       # if GE 2 prints out gaussian cube files with 
-                      # dielectric function, polarization charges, etc
-                      # WARNING: if GE 2 lot of I/O, much slower
-environ_thr='1.d0'    # electronic convergence threshold for the onset  
-                      # of solvation correction
+                      # electrostatic potential, electrolyte charge, etc.
+                      # WARNING: if GT 2 lot of I/O, much slower.
+environ_thr='1.d2'    # electronic convergence threshold for the onset
+                      # of solvation correction.
 ### ELECTROLYTE PARAMETERS ##########################################
-env_electrolyte_ntyp=2      # number of electrolyte species in solution
-zion=1                      # ionic charge
-cion='5.0'                  # ionic concentration (in mol/l)
-cionmax='15.0'              # max ionic concentration (in mol/l) 
-                            # NOTE: cionmax=1/a**3, where a is the ionic 
-                            # size parameter originally employed by Borukhov. 
-                            # one can alternatively set the ionic radius
-                            # rion (in Bohr), related to cionmax according
-                            # to: cionmax=p/(4/3*pi*rion**3), where p=0.64 
-                            # is the random close packing volume fraction.
-temperature=300             # temperature of the electrolyte solution 
+env_electrolyte_ntyp=2      # number of electrolyte species in solution.
+zion=1                      # ionic charge.
+cion='1.0'                  # ionic concentration (in mol/l).
+temperature=300             # temperature of the electrolyte solution.
 electrolyte_mode='system'   # specify the method that is used to build
-                            # the electrolyte cavity
+                            # the electrolyte cavity:
                             # electronic, ionic, full: same as for solvent
                             #   cavity, see previous examples. the 
                             #   corresponding parameters are 
                             #   electrolyte_rhomax, electrolyte_rhomin 
                             #   (for electronic and full), or
                             #   electrolyte_alpha and electrolyte_softness 
-                            #   (for ionic).
-                            # system: simplified erf-based interface 
+                            #   (for ionic);
+                            # system: simplified erf-based interface. 
 system_dim=2                # select dimensionality of the interface
                             # 0, 1 and 2: spherical, cylindrical or
-                            #   planar interface
+                            #   planar interface.
 system_axis=3               # select axis for the simplified interface.
                             # the axis is along the 1D direction or 
                             # normal to the 2D plane (1, 2 or 3 for 
-                            # x, y or z axis)
+                            # x, y or z axis).
 system_ntyp=1               # the geometrical center of the atoms of 
                             # all types up to system_ntyp defines the
-                            # origin of the simplified interface
-electrolyte_distance='10.0' # distance parameter for the simplified
-                            # erf-based interface
-electrolyte_spread='0.5'    # spread parameter for the simplified
-                            # erf-based interface 
-### PERIODIC BOUNDARY CONDITIONS ####################################
-pbc_correction='parabolic' # correction scheme to remove PBC 
-                           # none: periodic calculation, no correction 
-                           # parabolic: quadratic real-space correction
+                            # origin of the simplified interface.
+electrolyte_distance='30.0' # distance parameter for the simplified
+                            # erf-based interface.
+electrolyte_spread='0.01'   # spread parameter for the simplified
+                            # erf-based interface.
+### PERIODIC BOUNDARY CONDITIONS (PBC) ##############################
 pbc_dim=2                  # select the desired system dimensionality
-                           # 0, 1 or 2: isolated, 1D or 2D system
+                           # 0, 1 or 2: isolated, 1D or 2D system.
 pbc_axis=3                 # set the axis along the 1D direction or 
                            # normal to the 2D plane (pbc_axis = 1, 2 
-                           # or 3 for x, y or z axis)
+                           # or 3 for x, y or z axis).
 #####################################################################
 
-# clean TMP_DIR
-$ECHO "  cleaning $TMP_DIR...\c"
-rm -rf $TMP_DIR/*
-$ECHO " done"
-
-$ECHO "  running the scf calculation with a diffuse ionic charge"
-$ECHO "  density and $pbc_correction periodic boundary correction"
-
-prefix=PtCO_linearizedpb
+prefix='scf'
 input=${prefix}'.in'
 output=${prefix}'.out'
+rm -f results.txt
+
 cat > $input << EOF 
- &CONTROL
-   !
+&CONTROL
    calculation = 'scf'
    restart_mode = 'from_scratch'
    pseudo_dir = '$PSEUDO_DIR/'
    outdir = '$TMP_DIR/'
    prefix = '$prefix'
-   tprnfor = .TRUE.
+   tprnfor = .true.
    verbosity = 'high'
-   !
- /
- &SYSTEM
-   !
-   ibrav = 8
-   celldm(1) = 10.6881
-   celldm(2) = 0.866025
-   celldm(3) = 3.95422
-   nat = 10
-   ntyp = 3
-   ecutwfc = 35
-   ecutrho = 280
-   occupations = 'smearing'
-   degauss = 0.03
-   smearing = 'mv'
-   nbnd = 80
-   tot_charge = 1
-   !
 /
- &ELECTRONS
-   !
+&SYSTEM
+   ntyp             = 1
+   nat              = 8
+   ibrav            = 0
+   ecutwfc = 30
+   ecutrho = 300
+   occupations = 'smearing'
+   degauss = 0.02
+   smearing = 'mv'
+   tot_charge = 0.5
+/
+&ELECTRONS
    conv_thr = 5.D-9
    diagonalization = 'davidson'
-   mixing_beta = 0.2
-   electron_maxstep = 200
-   !
- /
-K_POINTS (automatic)
- 1 1 1 0 0 0
+!   mixing_beta = 0.1
+/
+&IONS
+/
+&CELL
+/
+
 ATOMIC_SPECIES
-Pt 1 Pt.pbe-nd-rrkjus.UPF
-C 1 C.pbe-rrkjus.UPF
-O 1 O.pbe-rrkjus.UPF
-ATOMIC_POSITIONS (bohr)
-Pt       8.061327071   0.098057998   8.992142901
-Pt       2.608989366   0.098058283   8.992140585
-Pt       0.000036609   4.720846294   8.968756935
-Pt       5.335159557   4.721612729   9.380196435
-Pt       0.000041121   7.802951963   4.604626508
-Pt       5.335161233   7.697749113   4.753489408
-Pt       2.697860636   3.152173889   4.688412329
-Pt       7.972463687   3.152174491   4.688415209
-C        5.335084148   4.646723426  12.901029877
-O        5.335009643   4.619623254  15.079854269
+Ag 107.8682 Ag.pbe-n-rrkjus_psl.1.0.0.UPF
+
+K_POINTS automatic
+2 2 1 0 0 0
+
+CELL_PARAMETERS angstrom
+2.93350761311527 0.00000000000000 0.00000000000000
+0.00000000000000 2.93350761311527 0.00000000000000
+0.00000000000000 0.00000000000000 54.44175455526538
+
+ATOMIC_POSITIONS angstrom
+Ag 1.4667538066 1.4667538066 20.0000000000
+Ag 0.0000000000 0.0000000000 22.0336703459
+Ag 1.4667538066 1.4667538066 24.1093139696
+Ag 0.0000000000 0.0000000000 26.1848761000
+Ag 1.4667538066 1.4667538066 28.2568784553
+Ag 0.0000000000 0.0000000000 30.3324405857
+Ag 1.4667538066 1.4667538066 32.4080836802
+Ag 0.0000000000 0.0000000000 34.4417545553
 EOF
-cat > environ.in << EOF 
- &ENVIRON
-   !
+
+for model in PB MPB ; do
+  # two electrolyte models are tested:
+  # PB  : standard Poisson-Boltzmann model, with point-like ions;
+  # MPB : size-modified PB model, with finite-sized ions.
+  #   the MPB model is activated through one of the following params:
+  #   - cionmax : parameter setting the maximum electrolyte concentration
+  #     (in mol/l); cionmax = 1/a**3, where a is the ionic size parameter
+  #     originally employed by Borukhov et al.
+  #   - rion : parameter corresponding to the ionic radius (in Bohr); it
+  #     is related to cionmax through: cionmax = p / (4/3*pi*rion**3),
+  #     where p=0.64 is the volume fraction for random close packing.
+
+  if [ $model == "PB" ] ; then
+      # standard PB model;
+      # for high ionic strengths and low surface charge the linear-regime
+      # approximation can be employed.
+      # available implementations: numerical and (2D)-analytic;
+      model="PB" ; cionmax=0
+      linearized="true false"
+      pbc_corrections="gcs parabolic"
+  else
+      # MPB model;
+      # available implementations: numerical only;
+      model="MPB" ; cionmax=10.0
+      pbc_corrections="parabolic"
+      linearized="false"
+  fi
+
+  for linear in $linearized ; do
+    # .true. : consider the linear-regime approximation of the PB model;
+    # .false. : solve the full non-linear PB equation.
+
+    if [ $linear == "true" ]; then
+       $ECHO "$model model (linearized) " >> results.txt
+    else 
+       $ECHO "$model model " >> results.txt
+    fi
+
+    for pbc_correction in $pbc_corrections ; do
+      # "parabolic" : real-space parabolic PBC correction; the (M)PB
+      #     equation is solved numerically using an iterative approach;
+      # "gcs" : Gouy-Chapman-Stern (GCS) correction; the PB equation is
+      #     solved analytically and imposed as boundary condition.
+      #     NOTE: only available for 2D systems!
+
+      # clean TMP_DIR
+      $ECHO "  cleaning $TMP_DIR...\c"
+      rm -rf $TMP_DIR/*
+      $ECHO " done"
+
+      $ECHO "  running the scf calculation with a diffuse ionic charge density;"
+      $ECHO "  the $model equation is solved with the $pbc_correction PBC correction"
+      $ECHO "  (linear-regime approximation: $linear);"
+
+      label="Ag100_model-${model}_pbccorr-${pbc_correction}_linear-${linear}"
+      cat > environ.in << EOF
+&ENVIRON
    verbose = $verbose
    environ_thr = $environ_thr
    environ_type = 'input'
@@ -230,30 +269,41 @@ cat > environ.in << EOF
    system_dim = $system_dim            
    system_axis = $system_axis   
    system_ntyp = $system_ntyp
-   electrolyte_linearized = .true.
-   !
- /
- &BOUNDARY
-   !
+   electrolyte_linearized = .$linear.
+/
+&BOUNDARY
    solvent_mode = 'full'
    electrolyte_mode = '$electrolyte_mode'
    electrolyte_distance = $electrolyte_distance
    electrolyte_spread = $electrolyte_spread   
-   !
- /
- &ELECTROSTATIC
-   !
+/
+&ELECTROSTATIC
    pbc_correction = '$pbc_correction'
    pbc_dim = $pbc_dim                 
    pbc_axis = $pbc_axis   
    tol = 5.D-13
-   !
- /
+   inner_tol = 5.D-18
+/
 EOF
-   
-$PW_COMMAND < $input > $output 
-check_failure $?
-$ECHO " done"
+      $PW_COMMAND < $input > $label.out
+      check_failure $?
+      mv environ.in $label.environ.in
+      $ECHO " done"
 
-$ECHO
-$ECHO "$EXAMPLE_DIR : done"
+      $ECHO
+      $ECHO "$EXAMPLE_DIR : done"
+    
+      fermi=$(awk '/the Fermi energy is/ {en=$5}; END {print en}' $label.out )
+      potshift=$(awk '/the potential shift due/ {en=$10}; END {print en}' $label.out )
+      pot=$($ECHO "-($fermi+$potshift)" | bc -l)
+
+      if [ $pbc_correction == "parabolic" ]; then
+         method="numerical solution   :"
+      else :
+         method="2D-analytic solution :"
+      fi
+      $ECHO "$method potential = -($fermi + $potshift) V = $pot V (abs. scale)" >> results.txt
+    done
+    $ECHO >> results.txt
+  done
+done
