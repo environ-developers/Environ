c6be3426107fde94db4e240f65726678e259732b
Francesco Nattino <francesco.nattino@epfl.ch>
2019-04-25 16:21:36 +0200

    Environ version number updated to 1.1

--- patches/environpatch.sh
+++ patches/environpatch.sh
@@ -2,14 +2,14 @@
 #
 # Copyright (C) 2018 ENVIRON (www.quantum-environment.org)
 #
-#    This file is part of Environ version 1.0
+#    This file is part of Environ version 1.1
 #
-#    Environ 1.0 is free software: you can redistribute it and/or modify
+#    Environ 1.1 is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 2 of the License, or
 #    (at your option) any later version.
 #
-#    Environ 1.0 is distributed in the hope that it will be useful,
+#    Environ 1.1 is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more detail, either the file
@@ -21,7 +21,7 @@
 # Author: Oliviero Andreussi (Department of Physics, UNT)
 #
 
-export ENVIRON_VERSION="1.0"
+export ENVIRON_VERSION="1.1"
 export ENVIRON_PATCH=$(cd "${BASH_SOURCE%/*}"; pwd)
 export ENVIRON_DIR="${ENVIRON_PATCH}/.."
 export ENVIRON_TEST="${ENVIRON_DIR}/tests"
