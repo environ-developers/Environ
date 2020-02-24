9b94354732e2bd34e3e94a6ee012c20caae142c9
drano1024 <gmedrano1024@gmail.com>
2020-02-24 15:12:58 -0600

    Adding in files from libraries and modified v_of_rho.f90 file.

---
+++ examples/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+EXEC="./run_example.sh"
+cd example01/
+$EXEC
+cd ../example02/
+$EXEC
+cd ../example04/
+$EXEC
+cd ../example05/
+$EXEC
+cd ../example06/
+$EXEC
+cd ../example07/
+$EXEC
+cd ../
