72b73b9bec5a42b0a89ff9f58488faba51cbc92a
drano1024 <gmedrano1024@gmail.com>
2022-04-26 13:03:26 -0500

    Removed debugging print statments

--- programs/prog_utils.f90
+++ programs/prog_utils.f90
@@ -122,31 +122,6 @@ CONTAINS
         END IF
         !
         !--------------------------------------------------------------------------------
-        ! Print summary
-        !
-        IF (io%lnode) THEN
-            WRITE (io%unit, 1000)
-            WRITE (io%unit, 1001)
-            WRITE (io%unit, 1002) at(1, 1), at(2, 2), at(3, 3)
-            WRITE (io%unit, 1003) nat
-            WRITE (io%unit, 1004) ntyp
-        END IF
-        !
-        FLUSH (io%unit)
-        !
-        !--------------------------------------------------------------------------------
-        !
-1000    FORMAT(5X, "Summary of system information")
-        !
-1001    FORMAT(10X, "Values will be printed using ENVIRON internal units, e.g. length is in Bohr")
-        !
-1002    FORMAT(10X, "Cell parameters: ", 3F17.8)
-        !
-1003    FORMAT(10X, "Number of atoms: ", I4)
-        !
-1004    FORMAT(10X, "Number of atomic types: ", I10)
-        !
-        !--------------------------------------------------------------------------------
     END SUBROUTINE init_environ_from_cube
     !------------------------------------------------------------------------------------
     !------------------------------------------------------------------------------------
